9fe0ee2b59f9c7bf0af8d965e9aa15f8fd118ba0
marcin witkowski <marcin.witkowski@e-qsg.com>
2018-10-03 20:18:37 +0200

    Final class code

---
+++ src/main/java/pracownia/introduction/Main.java
@@ -0,0 +1,14 @@
+package pracownia.introduction;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    final static Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String [ ] args) {
+        System.out.println("Hello World");
+
+        logger.info("Welcome to Programming Laboratory");
+    }
+}
---
+++ src/test/java/pracownia/introduction/MainTest.java
@@ -0,0 +1,5 @@
+package pracownia.introduction;
+
+public class MainTest {
+
+}
