f51e18f1b985c381fa7284e299a68cae1e515dd1
THINK <witkowski.mar@gmail.com>
2019-06-19 15:05:21 +0200

    Change

--- src/main/java/com/eureka/movie/movieservice/MovieServiceApplication.java
+++ src/main/java/com/eureka/movie/movieservice/MovieServiceApplication.java
@@ -3,13 +3,26 @@ package com.eureka.movie.movieservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableWebMvc
 @EnableDiscoveryClient //annotations to make the client working
 public class MovieServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieServiceApplication.class, args);
+	}
+
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
 }
