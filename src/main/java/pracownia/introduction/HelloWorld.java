0d6300ec4b7d21c50fe1ef35d7e32967618ac3e9
marcin witkowski <marcin.witkowski@e-qsg.com>
2019-10-03 17:33:39 +0200

    change to helloworld

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -2,9 +2,9 @@ package pracownia.introduction;
 
 import org.apache.log4j.Logger;
 
-public class Main {
+public class HelloWorld {
 
-    final static Logger logger = Logger.getLogger(Main.class);
+    final static Logger logger = Logger.getLogger(HelloWorld.class);
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
